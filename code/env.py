8300fe13ada66c2f247e18d60f1b38123cf68c64
MikkelFilip <mijens17@student.aau.dk>
2020-11-25 11:42:23 +0100

    added sending code

---
+++ code/env.py
@@ -0,0 +1,5 @@
+url = 'indoorclimate.tk'
+path = '/model/Measurement'
+PORT = '3000'
+cache = "cache"
+SensorId = 555
---
+++ code/sendInfo.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+import sys
+import requests
+import json
+from env import *
+from datetime import datetime
+
+t = datetime.now()
+
+data = {
+	'timestamp': '{}-{}-{} {}:{}:{}'.format(t.year, t.month, t.day, t.hour, t.minute, t.second),
+	'co2': sys.argv[1],
+	'humidity': sys.argv[2],
+	'temperature': sys.argv[3],
+	'sensorid': SensorId
+} 
+
+try:
+	r = requests.post("https://" + url + ":" + PORT + path, data=data, verify=False)
+except requests.exceptions.RequestException as e:
+	print(e)
+	f = open(cache, "a+")
+	f.write(json.dumps(data)+"\n")
+	f.close()
+
